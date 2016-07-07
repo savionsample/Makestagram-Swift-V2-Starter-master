f2a60f2264cc33299a6b1112e3021c7755f03d56
Savion <savsample@gmail.com>
2016-07-07 11:09:48 -0700

    initial push

---
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -0,0 +1,50 @@
+import UIKit
+import Parse
+
+class TimelineViewController: UIViewController {
+    var photoTakingHelper: PhotoTakingHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBarController?.delegate = self
+    }
+    
+//    func takePhoto() {
+//        
+    @IBOutlet weak var noTitle: UITableView!
+//        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!, callback: { (image: UIImage?) in
+//            if let image = image {
+//                let imageData = UIImageJPEGRepresentation(image, 0.8)!
+//                let imageFile = PFFile(name: "image.jpg", data: imageData)!
+//                
+//                let post = PFObject(className: "Post")
+//                post["imageFile"] = imageFile
+//                post.saveInBackground()
+//            }
+//        })
+//    }
+    
+    
+    func takePhoto() {
+        // instantiate photo taking class, provide callback for when photo is selected
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
+            let post = Post()
+            post.image = image
+            post.uploadPost()
+        }
+    }
+}
+
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
+    }}
